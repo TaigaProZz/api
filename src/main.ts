4789fcdc5d7654b2e08247b582586b3ec2146f54
Taiga ProZz <flo54@gmx.fr>
2024-07-08 00:43:35 +0200

    (feat) Swagger added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,14 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
+  
+  const config = new DocumentBuilder()
+    .setTitle('API JO')
+    .setDescription('API des jo (projet personnel)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
