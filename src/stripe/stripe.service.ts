083eda4a22e13b51805f95d357b9e9900547bfd9
Taiga ProZz <flo54@gmx.fr>
2024-07-17 14:16:22 +0200

    (fix) fix stripe service file

---
+++ src/stripe/stripe.service.ts
@@ -0,0 +1,95 @@
+import { Injectable, RawBodyRequest } from '@nestjs/common';
+import { response } from 'express';
+
+import Stripe from 'stripe';
+
+@Injectable()
+export class StripeService {
+  private stripe;
+
+  constructor() {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET);
+  }
+
+  async createCheckout(body: JSON, id: number) {
+    const stripePriceId = body['stripePriceId']<string>;
+
+    return await this.stripe.checkout.sessions.create({
+      line_items:
+        [{
+          price: stripePriceId,
+          quantity: 1,
+        }],
+      mode: 'payment',
+      success_url: `http://localhost:5173/?success=true`,
+      cancel_url: `http://localhost:5173/?canceled=true`,
+      metadata: {
+        userId: id,
+      }
+
+    });
+  }
+
+  async webhook(req: RawBodyRequest<Request>) {
+    const payload = req.rawBody;
+    const sig = req.headers['stripe-signature'];
+    let event;
+
+    // verify webhook signature
+    try {
+      event = this.stripe.webhooks.constructEvent(payload, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      console.log('erreur', err);
+      return `Webhook Error: ${err.message}`;
+    }
+
+    // handle events
+    switch (event.type) {
+      // payement success
+      case 'checkout.session.completed':
+        try {
+          const session = event.data.object;
+          const priceId = await this.getStripePriceId(session.id);
+          console.log("priceId: ", priceId);
+
+          // console.log("session: ", session);
+          return { userId: session.metadata.userId, stripePriceId: priceId };
+        } catch (error) {
+          console.log('error', error);
+        }
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    return;
+  }
+
+  async getStripePriceId(sessionId: string) {
+    const checkoutSession = await this.stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items'],
+    });
+
+    const lineItems = await this.stripe.checkout.sessions.listLineItems(
+      checkoutSession.id
+    );
+
+    return lineItems.data[0].price.id;
+  }
+
+
+  findAll() {
+    return `This action returns all stripe`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} stripe`;
+  }
+
+  update() {
+    return `This action updates a #${id} stripe`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} stripe`;
+  }
+}
